0100fb6e7d7bbb8b7a63b389943944beef2c363d
Vadim Lukin <lukinvadim101@gmail.com>
2023-05-21 19:18:09 -0600

    standart error

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -4,9 +4,17 @@ module ErrorHandler
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from ActionController::ParameterMissing, with: :parameter_missing
+    rescue_from StandardError, with: :handle_exception
   end
 
   private
+
+  def handle_exception(error)
+    logger.error { "#{error}: #{error.message}" }
+    logger.error error.backtrace.join("\n")
+
+    render json: { error: 'An unexpected error occurred', message: error.full_message }, status: :internal_server_error
+  end
 
   def record_not_found(error)
     render json: { error: 'Record not found', message: error.full_message }, status: :not_found
@@ -14,9 +22,5 @@ module ErrorHandler
 
   def parameter_missing(error)
     render json: { error: 'Missing parameter', message: error.full_message }, status: :unprocessable_entity
-  end
-
-  def faraday_error(error)
-    render json: { error: 'Error fetching data', message: error.full_message }, status: :unprocessable_entity
   end
 end
